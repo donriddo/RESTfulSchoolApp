939a91a53f88f5c303fab4b5311c859f5ec376e8
donriddo <donriddo@gmail.com>
2015-08-28 01:36:30 +0100

    Initial commit

--- api/controllers/StudentController.js
+++ api/controllers/StudentController.js
@@ -13,7 +13,7 @@ module.exports = {
 					if (err) console.log(err);
 					console.log(student, 'created');
 					req.session.authenticated = true;
-					delete student.password;
+					delete student.password; // saving this for later
 					student.who = 'student';
 					req.session.user = student;
 					res.redirect('/student/' + student.id);
